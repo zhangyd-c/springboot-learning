64c1801456dec6ee0f7d3677cbc72fa04e437c1d
yadong.zhang <yadong.zhang0415@gmail.com>
2017-03-10 16:13:23 +0800

    Update Applaction.java

--- springboot-schedule/src/main/java/com/zyd/schedule/Applaction.java
+++ springboot-schedule/src/main/java/com/zyd/schedule/Applaction.java
@@ -1,7 +1,6 @@
 package com.zyd.schedule;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
