39ff58d9039660a99b8f6094cc539afacaea6d03
yadong.zhang <yadong.zhang0415@gmail.com>
2017-03-10 16:15:38 +0800

    Update Applaction.java

--- springboot-mybatis/src/main/java/com/zyd/mybatis/Applaction.java
+++ springboot-mybatis/src/main/java/com/zyd/mybatis/Applaction.java
@@ -1,10 +1,8 @@
 package com.zyd.mybatis;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Applaction {
 	public static void main(String[] args) throws Exception {
