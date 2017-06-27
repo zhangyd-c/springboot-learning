f63c64207f5959b083a19ac596ce8fe7f520694d
yadong.zhang <yadong.zhang0415@gamil.com>
2017-06-27 18:14:41 +0800

    update

--- springboot-freemark/src/main/java/com/zyd/freemark/controller/FreemarkController.java
+++ springboot-freemark/src/main/java/com/zyd/freemark/controller/FreemarkController.java
@@ -1,9 +1,13 @@
 package com.zyd.freemark.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,12 +20,12 @@ public class FreemarkController {
 	public String index(Model model) {
 		return "index";
 	}
-	
+
 	@RequestMapping("/toUpload")
 	public String toUpload(Model model) {
 		return "upload";
 	}
-	
+
 	@RequestMapping("/upload")
 	@ResponseBody
 	public String upload(@RequestParam("file") MultipartFile[] files, String name,HttpServletRequest request) {
--- springboot-freemark/src/main/resources/templates/index.ftl
+++ springboot-freemark/src/main/resources/templates/index.ftl
@@ -6,7 +6,7 @@
 </head> 
 <body> 
 	<h1>Hello boy,</h1><br>
-	<p>当前时间：${.now?string("yyyy-MM-dd HH:mm:ss.sss")}</p> 
+	<p>当前时间：${.now?string("yyyy-MM-dd HH:mm:ss.sss")}</p>
 	<div class="body">
 		<h2>Eclipse中如何打开ftl文件？</h2>
 		<ul>
