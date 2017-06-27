e374f55db96a288f25b6fc414c65b179246add6f
yadong.zhang <yadong.zhang0415@gamil.com>
2017-06-27 18:07:53 +0800

    PageBean

--- springboot-mybatis/src/main/java/com/zyd/mybatis/controller/MyabatisController.java
+++ springboot-mybatis/src/main/java/com/zyd/mybatis/controller/MyabatisController.java
@@ -1,32 +1,30 @@
 package com.zyd.mybatis.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.zyd.mybatis.entity.Message;
+import com.zyd.mybatis.entity.PageBean;
+import com.zyd.mybatis.service.IMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.github.pagehelper.PageHelper;
-import com.zyd.mybatis.service.IMessageService;
-
 @Controller
 public class MyabatisController {
 
-	@Autowired
-	private IMessageService messageService;
+    @Autowired
+    private IMessageService messageService;
 
-	@RequestMapping("/{currentPage}/{pageSize}")
-	public String index(Model model, @PathVariable("currentPage") Integer currentPage,
-			@PathVariable("pageSize") Integer pageSize) {
-		if (currentPage != null) {
-			PageHelper.startPage(currentPage, pageSize);
-		}
-		model.addAttribute("list", messageService.list());
-		int total = messageService.count();
-		model.addAttribute("totalPage", total / pageSize + (total % pageSize > 0 ? 1 : 0));
-		model.addAttribute("pageSize", pageSize);
-		model.addAttribute("currentPage", currentPage);
-		return "index";
-	}
+    @RequestMapping("/{currentPage}/{pageSize}")
+    public String index(Model model, @PathVariable("currentPage") int currentPage,
+                        @PathVariable("pageSize") int pageSize) {
+
+        // 指定当前需要显示的页码（currentPage），页面列表长度（pageSize）
+        PageHelper.startPage(currentPage, pageSize);
+
+        model.addAttribute("page", new PageBean<Message>(messageService.list()));
+        return "index";
+    }
 
 }
