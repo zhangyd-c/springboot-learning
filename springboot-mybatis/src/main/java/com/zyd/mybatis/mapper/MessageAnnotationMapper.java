decb4f3408391e41975b65290135d9bd4a4fec7a
yadong.zhang <yadong.zhang0415@gamil.com>
2017-08-08 10:34:19 +0800

    支持注解的方式使用mybatis

---
+++ springboot-mybatis/src/main/java/com/zyd/mybatis/mapper/MessageAnnotationMapper.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright [2016-2017] [yadong.zhang]
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zyd.mybatis.mapper;
+
+import com.zyd.mybatis.entity.Message;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * springboot
+ * Created by yadong.zhang on com.zyd.mybatis.mapper
+ * @Author: yadong.zhang
+ * @Date: 2017/8/8 10:22
+ */
+@Mapper
+public interface MessageAnnotationMapper {
+
+    @Select("SELECT NICK_NAME FROM message")
+    @Results({
+            @Result(property = "nickName",  column = "NICK_NAME")
+    })
+    List<Message> list();
+}
