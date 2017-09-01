59468fea35095325bc21a77446ea40dfb29cd41b
yadong.zhang <yadong.zhang0415@gamil.com>
2017-09-01 16:13:54 +0800

    springboot集成mail

---
+++ springboot-mail/src/main/java/com/zyd/Applaction.java
@@ -0,0 +1,17 @@
+package com.zyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applaction {
+  	//env
+	//testmapper
+	//線程：dump  -- cmd:jconsole
+	//內存：
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Applaction.class, args);
+	}
+}
+
